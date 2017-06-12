8922254ff7efaa4e68b1d4c4c4af4db80c603683
Diarmuid Neligan <dneligan@agencyport.com>
2017-06-12 10:00:07 -0400

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'test/new'
+
+  root 'demo#index'
+
+  get 'demo/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
