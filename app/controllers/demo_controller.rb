6228aa950d02dbf2a88ee4bb3578f21d920a3cec
Diarmuid Neligan <dneligan@agencyport.com>
2017-06-12 11:21:13 -0400

    Git

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,11 +3,12 @@ class DemoController < ApplicationController
   layout false
 
   def index
-    render('hello')
+    render('index')
   end
 
   def hello
-    render('index')
+    @array = [1,2,99,4,5]
+    render('hello')
   end
 
   def other_hello
