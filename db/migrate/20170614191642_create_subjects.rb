ed3af944e5cc9d0e340d8a112ea9a61af3448817
Diarmuid Neligan <dneligan@agencyport.com>
2017-06-15 10:04:18 -0400

    changes

--- db/migrate/20170614191642_create_subjects.rb
+++ db/migrate/20170614191642_create_subjects.rb
@@ -1,9 +1,9 @@
 class CreateSubjects < ActiveRecord::Migration[5.1]
   def up
     create_table :subjects do |t|
-    t.column "name", :string, :limit => 25
-    t.integer "position", :limit => 50
-    t.boolean "visible", :default => "True", :null => false
+    t.string "name"
+    t.integer "position"
+    t.boolean "visible", :default => false
     t.timestamps
     t.timestamps
     end
--- db/migrate/20170614191705_create_sections.rb
+++ db/migrate/20170614191705_create_sections.rb
@@ -1,8 +1,21 @@
 class CreateSections < ActiveRecord::Migration[5.1]
-  def change
+  def up
     create_table :sections do |t|
-
+      t.integer "page_id"
+      t.string "name"
+      t.integer "position"
+      t.boolean "visible", :default => false,
+      t.string "content_type"
+      t.string "content"
+      t.timestamps
+    end
+    add_index("sections", "page_id")
       t.timestamps
     end
   end
+
+  def down 
+    drop_table :sections
+  end
+  
 end
